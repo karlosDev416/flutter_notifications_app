779d549100165b33f6e2e7fcb0bcb21555e4ad0a
Karlos Aguirre <karlos.dev416@gmail.com>
2023-02-13 22:43:29 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notifications_app/screens/home_screen.dart';
+import 'package:flutter_notifications_app/screens/message_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomeScreen(),
+        'message': (_) => MessageScreen(),
+      },
+    );
+  }
+}
